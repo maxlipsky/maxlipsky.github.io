8c203351cd32881f232a262f30e60a3905b717ab
Max Lipsky <maxlipsky@gmail.com>
2014-07-31 03:30:39 +0400

    test touch

--- main.js
+++ main.js
@@ -40,16 +40,17 @@
 
 
     document.addEventListener('touchstart', function(e) {
-        var touchObj = e.changedTouches[0],
-            startTouchScroll = parseInt(touchObj.clientY);
+        var touchObj = e.changedTouches[0];
+
+        startTouchScroll = parseInt(touchObj.clientY);
         e.preventDefault();
     }, false);
 
     document.addEventListener('touchmove', function(e) {
         var touchObj = e.changedTouches[0],
-            currentTouchScroll = startTouchScroll - parseInt(touchObj.clientY),
+            currentTouchScroll = startTouchScroll - parseInt(touchObj.clientY);
             currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-        window.scrollTo(0, currentScroll+currentTouchScroll);
+        window.scrollTo(0, currentTouchScroll);
         draw(x, y, side, steps, currentScroll);
         e.preventDefault();
     }, false);
